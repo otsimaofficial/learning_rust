e7e3f10039870e5433834ca64b6418189eec3d9a
otsimaofficial <iemmanuelogbu@gmail.com>
2025-10-02 13:49:17 +0100

    learning enums

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
-pub fn zeros() -> [u32; 100] {
-    [0; 100] // creates an array with 100 elements, all set to 0
+#[derive(Debug, PartialEq)]
+pub enum Color {
+    Red,
+    Green,
+    Blue,
+    Rgba(u8, u8, u8, f32),
 }
-
-pub fn first_3(s: &[u32]) -> &[u32] {
-    &s[..3] // slice from the beginning up to (but not including) index 3
-}
-
-pub fn last_3(s: &[u32]) -> &[u32] {
-    &s[s.len() - 3..] // slice from the third-to-last to the end
-}
+ // enum 2 
--- tests/test.rs
+++ tests/test.rs
@@ -1,22 +1,10 @@
-use array::*;
+use enum_type::*;
 
 #[test]
-fn test_zeros() {
-    let z = zeros();
-    assert_eq!(z.len(), 100);
-    for n in z {
-        assert_eq!(n, 0);
-    }
-}
+fn test_color() {
+    assert_eq!(Color::Red, Color::Red);
+    assert!(Color::Green != Color::Blue);
 
-#[test]
-fn test_first_3() {
-    let arr = [1, 2, 3, 4, 5, 6, 7];
-    assert_eq!(first_3(&arr), [1, 2, 3]);
-}
-
-#[test]
-fn test_last_3() {
-    let arr = [1, 2, 3, 4, 5, 6, 7];
-    assert_eq!(last_3(&arr), [5, 6, 7]);
+    // Check if it compiles
+    let _ = Color::Rgba(0, 255, 0, 0.5);
 }
