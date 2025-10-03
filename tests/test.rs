70cd586ca819ee425ceb11cf1675b1985da129c1
otsimaofficial <iemmanuelogbu@gmail.com>
2025-10-03 16:47:14 +0100

    learning functions call in rust

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,16 @@
-use error::*;
+use function::*;
 
 #[test]
-fn test_div() {
-    assert_eq!(div(1, 0), Err(MathError::DivByZero));
+fn test_mul() {
+    assert_eq!(mul(1, 0), 0);
+    assert_eq!(mul(0, 1), 0);
+    assert_eq!(mul(10, 2), 20);
+    assert_eq!(mul(13, 7), 91);
 }
 
 #[test]
-fn test_get() {
-    let v = vec![1, 2, 3];
-    assert_eq!(get(&v, 0, 0), 1);
-    assert_eq!(get(&v, 1, 0), 2);
-    assert_eq!(get(&v, 2, 0), 3);
-    assert_eq!(get(&v, 3, 0), 0);
+fn test_div() {
+    assert_eq!(div(0, 1), 0);
+    assert_eq!(div(10, 2), 5);
+    assert_eq!(div(13, 7), 1);
 }
